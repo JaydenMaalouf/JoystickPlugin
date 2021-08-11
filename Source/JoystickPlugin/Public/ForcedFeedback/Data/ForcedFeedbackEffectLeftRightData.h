627333d97aba2aedc2992a69689d60748fde1b96
JaydenMaalouf <jayden.m.maalouf@gmail.com>
2021-08-11 19:48:24 +1000

    Readded Length

--- Source/JoystickPlugin/Public/ForcedFeedback/Data/ForcedFeedbackEffectLeftRightData.h
+++ Source/JoystickPlugin/Public/ForcedFeedback/Data/ForcedFeedbackEffectLeftRightData.h
@@ -11,6 +11,9 @@ struct FForcedFeedbackEffectLeftRightData
 
 public:
 
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (ClampMin = "0", ClampMax = "4294967295", ShortToolTip = "Duration", ToolTip = "The duration of the effect."))
+		int64 Length = 1;
+
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (ClampMin = "0", ClampMax = "65535"))
 		int32 LargeMagnitude = 1;
 
