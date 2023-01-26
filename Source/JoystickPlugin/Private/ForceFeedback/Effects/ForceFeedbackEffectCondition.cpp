5208ef163d96a1cd1401356d927ad526462659a6
JaydenMaalouf <jayden.m.maalouf@gmail.com>
2023-01-26 15:38:26 +1100

    fix: :bug: compiler warning unable to differentiate the templated clamp

--- Source/JoystickPlugin/Private/ForceFeedback/Effects/ForceFeedbackEffectCondition.cpp
+++ Source/JoystickPlugin/Private/ForceFeedback/Effects/ForceFeedbackEffectCondition.cpp
@@ -35,26 +35,26 @@ void UForceFeedbackEffectCondition::UpdateEffectData()
 	}
 
 	Effect.condition.direction = EffectData.DirectionData.ToSDLDirection();
-	Effect.condition.length = EffectData.DurationData.InfiniteDuration ? SDL_HAPTIC_INFINITY : FMath::Clamp(EffectData.DurationData.Duration * 1000.0f, 0, UINT32_MAX);
-	Effect.condition.delay = FMath::Clamp(EffectData.ReplayData.Delay * 1000.0f, 0, UINT16_MAX);
-	Effect.condition.interval = FMath::Clamp(EffectData.ReplayData.RetriggerDelay * 1000.0f, 0, UINT16_MAX);
+	Effect.condition.length = EffectData.DurationData.InfiniteDuration ? SDL_HAPTIC_INFINITY : FMath::Clamp<Uint32>(EffectData.DurationData.Duration * 1000.0f, 0, UINT32_MAX);
+	Effect.condition.delay = FMath::Clamp<Uint16>(EffectData.ReplayData.Delay * 1000.0f, 0, UINT16_MAX);
+	Effect.condition.interval = FMath::Clamp<Uint16>(EffectData.ReplayData.RetriggerDelay * 1000.0f, 0, UINT16_MAX);
 
-	Effect.condition.right_sat[0] = FMath::Clamp(EffectData.RightSat.X * UINT16_MAX, 0, UINT16_MAX);
-	Effect.condition.right_sat[1] = FMath::Clamp(EffectData.RightSat.Y * UINT16_MAX, 0, UINT16_MAX);
-	Effect.condition.right_sat[2] = FMath::Clamp(EffectData.RightSat.Z * UINT16_MAX, 0, UINT16_MAX);
-	Effect.condition.left_sat[0] = FMath::Clamp(EffectData.LeftSat.X * UINT16_MAX, 0, UINT16_MAX);
-	Effect.condition.left_sat[1] = FMath::Clamp(EffectData.LeftSat.Y * UINT16_MAX, 0, UINT16_MAX);
-	Effect.condition.left_sat[2] = FMath::Clamp(EffectData.LeftSat.Z * UINT16_MAX, 0, UINT16_MAX);
-	Effect.condition.right_coeff[0] = FMath::Clamp(EffectData.RightCoefficient.X * INT16_MAX, INT16_MIN, INT16_MAX);
-	Effect.condition.right_coeff[1] = FMath::Clamp(EffectData.RightCoefficient.Y * INT16_MAX, INT16_MIN, INT16_MAX);
-	Effect.condition.right_coeff[2] = FMath::Clamp(EffectData.RightCoefficient.Z * INT16_MAX, INT16_MIN, INT16_MAX);
-	Effect.condition.left_coeff[0] = FMath::Clamp(EffectData.LeftCoefficient.X * INT16_MAX, INT16_MIN, INT16_MAX);
-	Effect.condition.left_coeff[1] = FMath::Clamp(EffectData.LeftCoefficient.Y * INT16_MAX, INT16_MIN, INT16_MAX);
-	Effect.condition.left_coeff[2] = FMath::Clamp(EffectData.LeftCoefficient.Z * INT16_MAX, INT16_MIN, INT16_MAX);
-	Effect.condition.deadband[0] = FMath::Clamp(EffectData.DeadZone.X * UINT16_MAX, 0, UINT16_MAX);
-	Effect.condition.deadband[1] = FMath::Clamp(EffectData.DeadZone.Y * UINT16_MAX, 0, UINT16_MAX);
-	Effect.condition.deadband[2] = FMath::Clamp(EffectData.DeadZone.Z * UINT16_MAX, 0, UINT16_MAX);
-	Effect.condition.center[0] = FMath::Clamp(EffectData.Center.X * INT16_MAX, INT16_MIN, INT16_MAX);
-	Effect.condition.center[1] = FMath::Clamp(EffectData.Center.Y * INT16_MAX, INT16_MIN, INT16_MAX);
-	Effect.condition.center[2] = FMath::Clamp(EffectData.Center.Z * INT16_MAX, INT16_MIN, INT16_MAX);
+	Effect.condition.right_sat[0] = FMath::Clamp<Uint16>(EffectData.RightSat.X * UINT16_MAX, 0, UINT16_MAX);
+	Effect.condition.right_sat[1] = FMath::Clamp<Uint16>(EffectData.RightSat.Y * UINT16_MAX, 0, UINT16_MAX);
+	Effect.condition.right_sat[2] = FMath::Clamp<Uint16>(EffectData.RightSat.Z * UINT16_MAX, 0, UINT16_MAX);
+	Effect.condition.left_sat[0] = FMath::Clamp<Uint16>(EffectData.LeftSat.X * UINT16_MAX, 0, UINT16_MAX);
+	Effect.condition.left_sat[1] = FMath::Clamp<Uint16>(EffectData.LeftSat.Y * UINT16_MAX, 0, UINT16_MAX);
+	Effect.condition.left_sat[2] = FMath::Clamp<Uint16>(EffectData.LeftSat.Z * UINT16_MAX, 0, UINT16_MAX);
+	Effect.condition.right_coeff[0] = FMath::Clamp<Sint16>(EffectData.RightCoefficient.X * INT16_MAX, INT16_MIN, INT16_MAX);
+	Effect.condition.right_coeff[1] = FMath::Clamp<Sint16>(EffectData.RightCoefficient.Y * INT16_MAX, INT16_MIN, INT16_MAX);
+	Effect.condition.right_coeff[2] = FMath::Clamp<Sint16>(EffectData.RightCoefficient.Z * INT16_MAX, INT16_MIN, INT16_MAX);
+	Effect.condition.left_coeff[0] = FMath::Clamp<Sint16>(EffectData.LeftCoefficient.X * INT16_MAX, INT16_MIN, INT16_MAX);
+	Effect.condition.left_coeff[1] = FMath::Clamp<Sint16>(EffectData.LeftCoefficient.Y * INT16_MAX, INT16_MIN, INT16_MAX);
+	Effect.condition.left_coeff[2] = FMath::Clamp<Sint16>(EffectData.LeftCoefficient.Z * INT16_MAX, INT16_MIN, INT16_MAX);
+	Effect.condition.deadband[0] = FMath::Clamp<Uint16>(EffectData.DeadZone.X * UINT16_MAX, 0, UINT16_MAX);
+	Effect.condition.deadband[1] = FMath::Clamp<Uint16>(EffectData.DeadZone.Y * UINT16_MAX, 0, UINT16_MAX);
+	Effect.condition.deadband[2] = FMath::Clamp<Uint16>(EffectData.DeadZone.Z * UINT16_MAX, 0, UINT16_MAX);
+	Effect.condition.center[0] = FMath::Clamp<Sint16>(EffectData.Center.X * INT16_MAX, INT16_MIN, INT16_MAX);
+	Effect.condition.center[1] = FMath::Clamp<Sint16>(EffectData.Center.Y * INT16_MAX, INT16_MIN, INT16_MAX);
+	Effect.condition.center[2] = FMath::Clamp<Sint16>(EffectData.Center.Z * INT16_MAX, INT16_MIN, INT16_MAX);
 }
--- Source/JoystickPlugin/Private/ForceFeedback/Effects/ForceFeedbackEffectCustom.cpp
+++ Source/JoystickPlugin/Private/ForceFeedback/Effects/ForceFeedbackEffectCustom.cpp
@@ -8,23 +8,23 @@ void UForceFeedbackEffectCustom::UpdateEffectData()
 	//effect.custom.type = SDL_HAPTIC_CUSTOM;
 
 	Effect.custom.direction = EffectData.DirectionData.ToSDLDirection();
-	Effect.custom.length = EffectData.DurationData.InfiniteDuration ? SDL_HAPTIC_INFINITY : FMath::Clamp(EffectData.DurationData.Duration * 1000.0f, 0, UINT32_MAX);
-	Effect.custom.delay = FMath::Clamp(EffectData.ReplayData.Delay * 1000.0f, 0, UINT16_MAX);
-	Effect.custom.interval = FMath::Clamp(EffectData.ReplayData.RetriggerDelay * 1000.0f, 0, UINT16_MAX);
+	Effect.custom.length = EffectData.DurationData.InfiniteDuration ? SDL_HAPTIC_INFINITY : FMath::Clamp<Uint32>(EffectData.DurationData.Duration * 1000.0f, 0, UINT32_MAX);
+	Effect.custom.delay = FMath::Clamp<Uint16>(EffectData.ReplayData.Delay * 1000.0f, 0, UINT16_MAX);
+	Effect.custom.interval = FMath::Clamp<Uint16>(EffectData.ReplayData.RetriggerDelay * 1000.0f, 0, UINT16_MAX);
 
-	Effect.custom.channels = FMath::Clamp(EffectData.Channels, static_cast<Uint8>(0), UINT8_MAX);
-	Effect.custom.period = FMath::Clamp(EffectData.Period * UINT16_MAX, 0, UINT16_MAX);
-	Effect.custom.samples = FMath::Clamp(static_cast<Uint16>(EffectData.Samples), static_cast<Uint16>(0), UINT16_MAX);
+	Effect.custom.channels = FMath::Clamp<Uint8>(EffectData.Channels, 0, UINT8_MAX);
+	Effect.custom.period = FMath::Clamp<Uint16>(EffectData.Period * UINT16_MAX, 0, UINT16_MAX);
+	Effect.custom.samples = FMath::Clamp<Uint16>(EffectData.Samples, 0, UINT16_MAX);
 
 	Uint16* DataArray = new Uint16[EffectData.Data.Num()]();
 	for (int i = 0; i < EffectData.Data.Num(); i++)
 	{
-		DataArray[i] = FMath::Clamp(static_cast<Uint16>(EffectData.Data[i]), static_cast<Uint16>(0), UINT16_MAX);
+		DataArray[i] = FMath::Clamp<Uint16>(EffectData.Data[i], 0, UINT16_MAX);
 	}
 	Effect.custom.data = DataArray;
 
-	Effect.custom.attack_length = FMath::Clamp(EffectData.EnvelopeData.AttackDuration * 1000.0f, 0, UINT16_MAX);
-	Effect.custom.attack_level = FMath::Clamp(EffectData.EnvelopeData.AttackLevel * UINT16_MAX, 0, UINT16_MAX);
-	Effect.custom.fade_length = FMath::Clamp(EffectData.EnvelopeData.FadeDuration * 1000.0f, 0, UINT16_MAX);
-	Effect.custom.fade_level = FMath::Clamp(EffectData.EnvelopeData.FadeLevel * UINT16_MAX, 0, UINT16_MAX);
+	Effect.custom.attack_length = FMath::Clamp<Uint16>(EffectData.EnvelopeData.AttackDuration * 1000.0f, 0, UINT16_MAX);
+	Effect.custom.attack_level = FMath::Clamp<Uint16>(EffectData.EnvelopeData.AttackLevel * UINT16_MAX, 0, UINT16_MAX);
+	Effect.custom.fade_length = FMath::Clamp<Uint16>(EffectData.EnvelopeData.FadeDuration * 1000.0f, 0, UINT16_MAX);
+	Effect.custom.fade_level = FMath::Clamp<Uint16>(EffectData.EnvelopeData.FadeLevel * UINT16_MAX, 0, UINT16_MAX);
 }
