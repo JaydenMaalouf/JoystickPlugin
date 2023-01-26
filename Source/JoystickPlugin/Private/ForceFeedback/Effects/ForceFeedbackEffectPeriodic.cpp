1fdad10030014c37b0938221558c81e21ce14f4e
JaydenMaalouf <jayden.m.maalouf@gmail.com>
2023-01-26 15:38:26 +1100

    fix: :bug: compiler warning unable to differentiate the templated clamp

--- Source/JoystickPlugin/Private/ForceFeedback/Effects/ForceFeedbackEffectPeriodic.cpp
+++ Source/JoystickPlugin/Private/ForceFeedback/Effects/ForceFeedbackEffectPeriodic.cpp
@@ -41,17 +41,17 @@ void UForceFeedbackEffectPeriodic::UpdateEffectData()
 	}
 
 	Effect.periodic.direction = EffectData.DirectionData.ToSDLDirection();
-	Effect.periodic.length = EffectData.DurationData.InfiniteDuration ? SDL_HAPTIC_INFINITY : FMath::Clamp(EffectData.DurationData.Duration * 1000.0f, 0, UINT32_MAX);
-	Effect.periodic.delay = FMath::Clamp(EffectData.ReplayData.Delay * 1000.0f, 0, UINT16_MAX);
-	Effect.periodic.interval = FMath::Clamp(EffectData.ReplayData.RetriggerDelay * 1000.0f, 0, UINT16_MAX);
+	Effect.periodic.length = EffectData.DurationData.InfiniteDuration ? SDL_HAPTIC_INFINITY : FMath::Clamp<Uint32>(EffectData.DurationData.Duration * 1000.0f, 0, UINT32_MAX);
+	Effect.periodic.delay = FMath::Clamp<Uint16>(EffectData.ReplayData.Delay * 1000.0f, 0, UINT16_MAX);
+	Effect.periodic.interval = FMath::Clamp<Uint16>(EffectData.ReplayData.RetriggerDelay * 1000.0f, 0, UINT16_MAX);
 
-	Effect.periodic.period = FMath::Clamp(EffectData.Period * 1000.0f, 0, UINT16_MAX);
-	Effect.periodic.magnitude = FMath::Clamp(EffectData.Magnitude * INT16_MAX, INT16_MIN, INT16_MAX);
-	Effect.periodic.offset = FMath::Clamp(EffectData.Offset * INT16_MAX, INT16_MIN, INT16_MAX);
-	Effect.periodic.phase = FMath::Clamp(EffectData.Phase * 100.0f, 0, UINT16_MAX);
+	Effect.periodic.period = FMath::Clamp<Uint16>(EffectData.Period * 1000.0f, 0, UINT16_MAX);
+	Effect.periodic.magnitude = FMath::Clamp<Sint16>(EffectData.Magnitude * INT16_MAX, INT16_MIN, INT16_MAX);
+	Effect.periodic.offset = FMath::Clamp<Sint16>(EffectData.Offset * INT16_MAX, INT16_MIN, INT16_MAX);
+	Effect.periodic.phase = FMath::Clamp<Uint16>(EffectData.Phase * 100.0f, 0, UINT16_MAX);
 
-	Effect.periodic.attack_length = FMath::Clamp(EffectData.EnvelopeData.AttackDuration * 1000.0f, 0, UINT16_MAX);
-	Effect.periodic.attack_level = FMath::Clamp(EffectData.EnvelopeData.AttackLevel * UINT16_MAX, 0, UINT16_MAX);
-	Effect.periodic.fade_length = FMath::Clamp(EffectData.EnvelopeData.FadeDuration * 1000.0f, 0, UINT16_MAX);
-	Effect.periodic.fade_level = FMath::Clamp(EffectData.EnvelopeData.FadeLevel * UINT16_MAX, 0, UINT16_MAX);
+	Effect.periodic.attack_length = FMath::Clamp<Uint16>(EffectData.EnvelopeData.AttackDuration * 1000.0f, 0, UINT16_MAX);
+	Effect.periodic.attack_level = FMath::Clamp<Uint16>(EffectData.EnvelopeData.AttackLevel * UINT16_MAX, 0, UINT16_MAX);
+	Effect.periodic.fade_length = FMath::Clamp<Uint16>(EffectData.EnvelopeData.FadeDuration * 1000.0f, 0, UINT16_MAX);
+	Effect.periodic.fade_level = FMath::Clamp<Uint16>(EffectData.EnvelopeData.FadeLevel * UINT16_MAX, 0, UINT16_MAX);
 }
