215dfc211985c134bba75f8347e001cb77903023
Jayden Maalouf <jayden.m.maalouf@gmail.com>
2024-07-28 18:54:11 +1000

    fix(force-feedback): :bug: build issue

--- Source/JoystickPlugin/Private/JoystickInputDevice.cpp
+++ Source/JoystickPlugin/Private/JoystickInputDevice.cpp
@@ -36,7 +36,7 @@ void FJoystickInputDevice::SetChannelValue(int ControllerId, FForceFeedbackChann
 void FJoystickInputDevice::SetChannelValues(int ControllerId, const FForceFeedbackValues& Values)
 {
 #if ENGINE_MAJOR_VERSION == 5
-	const UJoystickHapticDeviceManager* HapticDeviceManager = UJoystickHapticDeviceManager::GetJoystickHapticDeviceManager();
+	UJoystickHapticDeviceManager* HapticDeviceManager = UJoystickHapticDeviceManager::GetJoystickHapticDeviceManager();
 	if (!IsValid(HapticDeviceManager))
 	{
 		return;
