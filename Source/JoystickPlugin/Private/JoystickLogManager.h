cbf47c8a55fe6c1722fb2c27d26eee80a08c9002
Jayden Maalouf <jayden.m.maalouf@gmail.com>
2025-06-15 12:54:46 +1000

    fix: :bug: missing version include

--- Source/JoystickPlugin/Private/JoystickLogManager.h
+++ Source/JoystickPlugin/Private/JoystickLogManager.h
@@ -3,9 +3,10 @@
 
 #pragma once
 
-#include "Misc/Build.h"
 #include "Data/ResultMessage.h"
 #include "JoystickInputSettings.h"
+#include "Misc/Build.h"
+#include "Runtime/Launch/Resources/Version.h"
 
 #if ENGINE_MAJOR_VERSION == 5 && ENGINE_MINOR_VERSION >= 6
 	#include "Templates/CheckedFormatString.h"
